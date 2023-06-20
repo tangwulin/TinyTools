ef41d523a6380ad2d2503020cdd14c69113e8c54
tangwulin <3498665850@qq.com>
2023-06-20 13:26:01 +0800

    修正补位策略，优化添加功能

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -19,33 +19,9 @@
         <NButton>保存</NButton>
       </NButtonGroup>
       <n-button-group>
-        <n-button @click="showAddModal=true">增加人员</n-button>
-        <n-button @click="showAddMultiModal=true">批量增加人员</n-button>
+        <n-button @click="showAddMultiModal=true">增加人员</n-button>
       </n-button-group>
     </div>
-
-    <n-modal v-model:show="showAddModal">
-      <n-card
-          style="width: 50%"
-          title="增加人员"
-          :bordered="true"
-          size="huge"
-          role="dialog"
-          aria-modal="true"
-      >
-        <n-form :label-width="80" :model="formValue" :rules="rules">
-          <n-form-item label="姓名" path="name">
-            <n-input v-model:value="formValue.name" placeholder="姓名"/>
-          </n-form-item>
-          <n-form-item label="性别" path="sex">
-            <n-select v-model:value="formValue.sex" :options="sexOption" placeholder="选择性别"/>
-          </n-form-item>
-        </n-form>
-        <template #footer>
-          尾部
-        </template>
-      </n-card>
-    </n-modal>
 
     <n-modal v-model:show="showAddMultiModal">
       <n-card
@@ -61,17 +37,19 @@
             :label-width="80"
             :model="formMultiValue"
         >
-          <n-form-item label="请在下方输入姓名，以空格或英文逗号分割" path="input">
-            <n-input v-model:value="formMultiValue.input" type="textarea" placeholder="以空格或逗号分割的多个姓名"
+          <n-form-item label="请在下方输入姓名，多个请以空格或英文逗号分割" path="input">
+            <n-input v-model:value="formMultiValue.input" type="textarea" placeholder="这里可以输入多个"
                      @blur="parseName" @focus="parseName" @keyup="parseName"/>
           </n-form-item>
           <n-form-item label="解析到的姓名" path="names">
             <n-dynamic-tags v-model:value="formMultiValue.names"/>
           </n-form-item>
         </n-form>
-        <template #footer >
+        <template #footer>
           <div class="flex">
-            <NButton class="ml-auto" type="primary" @click="addPerson">保存</NButton>
+            <NButton class="ml-auto" type="primary" @click="addPerson" :disabled="formMultiValue.names.length===0">
+              保存
+            </NButton>
           </div>
         </template>
       </n-card>
@@ -81,40 +59,30 @@
 </template>
 
 <script setup>
-import { NButton, NModal, NCard, NForm, NFormItem, NInput, NSelect, NDynamicTags, useMessage } from 'naive-ui';
+import { NButton, NModal, NCard, NForm, NFormItem, NInput, NDynamicTags, useMessage } from 'naive-ui';
 import draggable from "vuedraggable-swap";
-//import { useSeatStore } from "@/stores/seats";
+import { useSeatStore } from "@/stores/seats";
 import { usePersonStore } from "@/stores/person";
 import { computed, ref } from "vue";
 import { storeToRefs } from "pinia";
 
-const message = useMessage()
-const showAddModal = ref(false);
+const message = useMessage();
 const showAddMultiModal = ref(false);
-
-const formValue = ref({ name: "", sex: "" });
 
 const formMultiValue = ref({ input: '', names: [] });
 
 const parseName = () => {
-  formMultiValue.value.names = formMultiValue.value.input.split(/[,\s]+/);
+  formMultiValue.value.names =
+      formMultiValue.value.input
+          .split(/[,\s]+/)
+          .filter((element) => element !== undefined && element !== null && element !== "");
 };
-
-const sexOption = [
-  {
-    label: "男",
-    value: "男",
-  },
-  {
-    label: "女",
-    value: "女",
-  }
-];
-const rules = { name: { required: true, message: "请输入姓名", trigger: "blur" } };
 
 const getRenderingList = (x) => {
   let result;
-  result = x.value.map((name)=>{return { name: name, isSeat: true }}).flatMap((value, index) => {
+  result = x.value.map((name, index) => {
+    return { name: name, isSeat: true, index: index };
+  }).flatMap((value, index) => {
     if ((index + 1) % 2 === 0 && (index + 1) % 8 !== 0)
     {
       return [value, { name: null, isSeat: false }];
@@ -123,12 +91,13 @@ const getRenderingList = (x) => {
       return value;
     }
   });
-
-  let remaining = Math.ceil(11 - (result.length / 11) + 1);
-
+  //console.log('result.length:'+result.length)
+  let remaining = 11 - Math.ceil(result.length % 11);
+  //console.log('remaining:'+remaining);
+  //console.log(Math.ceil(remaining % 3))
   if (remaining % 3 !== 0)
   {
-    for (let i = 0; i < remaining % 3; i++)
+    for (let i = 0; i < Math.ceil(remaining % 3); i++)
     {
       result.push({ name: null, isSeat: false, isDashed: true });
     }
@@ -136,33 +105,34 @@ const getRenderingList = (x) => {
 
   for (let i = 0; i < (remaining - remaining % 3) / 3; i++)
   {
-    result.push({ name: null, isSeat: false }, { name: null, isSeat: false, isDashed: true }, {
-      name: null,
-      isSeat: false,
-      isDashed: true
-    });
+    result.push(
+        { name: null, isSeat: false },
+        { name: null, isSeat: false, isDashed: true },
+        { name: null, isSeat: false, isDashed: true });
   }
 
   return result;
 };
 
-//const seatStore = useSeatStore()
+const seatStore = useSeatStore();
 const personStore = usePersonStore();
-//const { allSeats, whoInEdgeSeats } = storeToRefs(seatStore)
+const { allSeats, whoInEdgeSeats } = storeToRefs(seatStore);
 const { allPerson } = storeToRefs(personStore);
 
+const parseEdgeSeat = (x) => {
+  let result = [];
+  result.concat(x.slice(0, 11));
+  return result;
+};
 const addPerson = () => {
-  if(formMultiValue.value.names.length===0) message.warning("至少添加一个")
-  allPerson.value.push(...formMultiValue.value.names)
-  //personList.value=allPerson.value
-  //renderingList.value = getRenderingList(personList)
-  message.success("添加成功，共添加了"+formMultiValue.value.names.length+"个")
-  console.log(renderingList.value)
-  showAddMultiModal.value=false
-}
-//const personList = ref(getList(60));
+  //if(formMultiValue.value.names.length===0) message.warning("至少添加一个")
+  allPerson.value.push(...formMultiValue.value.names);
+  message.success("添加成功，共添加了" + formMultiValue.value.names.length + "个");
+  showAddMultiModal.value = false;
+  formMultiValue.value.names = [];
+};
 const personList = allPerson;
-const renderingList = computed(()=>getRenderingList(personList))
+const renderingList = computed(() => getRenderingList(personList));
 
 const reSort = () => {
   personList.value = shuffleArray(personList.value);
