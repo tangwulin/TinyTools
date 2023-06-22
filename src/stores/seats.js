70311e15ea01ef16b8e7719299f1101415c569bc
tangwulin <3498665850@qq.com>
2023-06-22 11:45:35 +0800

    数据结构改动

--- src/stores/seats.js
+++ src/stores/seats.js
@@ -1,18 +1,18 @@
-import { computed, ref } from "vue";
+import {  ref } from "vue";
 import { defineStore } from "pinia";
 
 
 export const useSeatStore = defineStore('seat', () => {
         const allSeats = ref([])
         const edgeSeatsIndex = ref([])
-        const whoInEdgeSeats = computed(edgeSeatsIndex.value.map(index => allSeats.value[index]))
+        //const whoInEdgeSeats = computed(edgeSeatsIndex.value.map(index => allSeats.value[index]))
 
         /*function increment()
         {
             count.value++
         }*/
 
-        return { allSeats, edgeSeatsIndex,whoInEdgeSeats }
+        return { allSeats, edgeSeatsIndex }
     },
     {
         persist: true,
