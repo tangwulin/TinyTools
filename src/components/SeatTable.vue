d7d8f11b1425ec269b9f56cd8fde47c9b6b2e8de
tangwulin <3498665850@qq.com>
2023-06-22 11:47:43 +0800

    增加性 能 监 测，增加了列表更新时原座位数据和渲染数据的互锁

--- src/components/SeatTable.vue
+++ src/components/SeatTable.vue
@@ -4,25 +4,30 @@ import draggable from "vuedraggable-swap";
 import { computed, ref, watch } from "vue";
 
 const props = defineProps(['seats']);
-const emit = defineEmits(['update','update:seats']);
+const emit = defineEmits(['update', 'update:seats']);
 
-//const seats = ref(props.seats);
-const seats =computed({
-  get(){
-    return props.seats
+const seatsUpdating=ref(false)
+const seats = computed({
+  get()
+  {
+    return props.seats;
   },
-  set(value){
-    emit('update:seats',value)
+  set(value)
+  {
+    emit('update:seats', value);
   }
-})
-
+});
 
 const getRenderingList = (x) => {
-  let result = [];
-  if (x.length === 0) return result;
-  result = x.map(item => {
-    return { name: item.name, isSeat: true, index: item.index };
-  }).flatMap((value, index) => {
+  let stopwatch = performance.now();
+  if (x.length === 0) return [];
+
+  const result = x.map(item => ({
+    name: item.name,
+    isSeat: true,
+    index: item.index,
+    color: item.color
+  })).flatMap((value, index) => {
     if ((index + 1) % 2 === 0 && (index + 1) % 8 !== 0)
     {
       return [value, { name: null, isSeat: false }];
@@ -31,12 +36,13 @@ const getRenderingList = (x) => {
       return value;
     }
   });
-
-  let remaining = 11 - Math.ceil(result.length % 11);
+  console.log('convert time:' + (performance.now() - stopwatch));
+  stopwatch = performance.now();
+  const remaining = 11 - (result.length % 11);
 
   if (remaining % 3 !== 0)
   {
-    for (let i = 0; i < Math.ceil(remaining % 3); i++)
+    for (let i = 0; i < remaining % 3; i++)
     {
       result.push({ name: null, isSeat: false, isDashed: true });
     }
@@ -47,9 +53,10 @@ const getRenderingList = (x) => {
     result.push(
         { name: null, isSeat: false },
         { name: null, isSeat: false, isDashed: true },
-        { name: null, isSeat: false, isDashed: true });
+        { name: null, isSeat: false, isDashed: true }
+    );
   }
-
+  console.log('add blank time:' + (performance.now() - stopwatch));
   return result;
 };
 
@@ -58,25 +65,38 @@ const parseRenderingListToSeats = (x) => {
   return x.map(item => {
     if (item.isSeat !== false)
     {
-      return { name: item.name, index: item.index };
+      return { name: item.name, index: item.index, color: item.color };
     }
   }).filter(item => item !== undefined);
 };
 
-const renderingList = ref(getRenderingList(seats.value));
-
-watch(renderingList, async () => {
-  seats.value = parseRenderingListToSeats(renderingList.value);
-  emit('update')
+const renderingListRef = ref(getRenderingList(seats.value));
+const renderingList = computed({
+  get()
+  {
+    return renderingListRef.value;
+  },
+  set(value)
+  {
+    renderingListRef.value = value;
+    seatsUpdating.value=true
+    seats.value = parseRenderingListToSeats(renderingListRef.value);
+    console.log('renderingList updated');
+    emit('update');
+  }
 });
 
+watch(seats, () => {
+  if (seatsUpdating.value===true) return
+  renderingList.value = getRenderingList(seats.value);
+});
 
 </script>
 
 <template>
   <div>
     <div class="flex items-center justify-center h-1/2">
-      <draggable v-model="renderingList" filter=".should-not-be-dragged" :swap="true" animation="150"
+      <draggable v-model="renderingList" filter=".should-not-be-dragged" :swap="true" animation="50"
                  class="text-center w-2/3 h-1/2 grid grid-cols-11" item-key="id">
         <!--suppress VueUnrecognizedSlot -->
         <template #item="{ element }">
