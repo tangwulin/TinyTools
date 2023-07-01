3877bbe9bf25bef86cf93f230a4fbcdb99794bcd
tangwulin <3498665850@qq.com>
2023-07-01 23:38:57 +0800

    自动导入组件

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
-import { visualizer } from "rollup-plugin-visualizer";
+import { visualizer } from 'rollup-plugin-visualizer'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer()],
+  plugins: [
+    vue(),
+    visualizer(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -16,9 +38,11 @@ export default defineConfig({
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/.pnpm/')[1].split('/')[0].toString();
+        manualChunks(id)
+        {
+          if (id.includes('node_modules'))
+          {
+            return id.toString().split('node_modules/.pnpm/')[1].split('/')[0].toString()
           }
         }
       }
