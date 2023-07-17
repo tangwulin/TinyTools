ad2f65fdd312e301676bd588b51520d0e1d8b892
tangwulin <3498665850@qq.com>
2023-07-17 22:35:06 +0800

    增加splashscreen支持(这东西，release版本启动快的一批，爱要不要

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SeatView from '../views/SeatView.vue'
+import { appWindow, WebviewWindow } from '@tauri-apps/api/window'
+
+const key = 'isBooting'
+const value = sessionStorage.getItem(key)
+
+if (value === null || value === undefined)
+{
+  // key 在 sessionStorage 中不存在或者值为 null/undefined
+  sessionStorage.setItem(key, 'true')
+}
+let isBooting = JSON.parse(sessionStorage.getItem(key))
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +26,33 @@ const router = createRouter({
       component: () => import('../views/TransparentView.vue')
     }
   ]
+})
+
+const openMainWindow = async () => {
+  isBooting = false
+  sessionStorage.setItem(key, 'false')
+  try
+  {
+    const splashscreen = WebviewWindow.getByLabel('splashscreen')
+    if (splashscreen !== null)
+    {
+      await splashscreen.close()
+      console.log('splashscreen关闭成功')
+    }
+  }
+  catch (e)
+  {
+    sessionStorage.setItem('error', JSON.stringify(e))
+  }
+  await appWindow.show()
+}
+router.afterEach(async (to, from) => {
+  // 在路由导航成功完成后执行的逻辑
+  console.log(isBooting)
+  if (isBooting)
+  {
+    setTimeout(() => {openMainWindow()}, 200)
+  }
 })
 
 export default router
