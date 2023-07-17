4211e33f70ccc81d98f8b61983f097c5ed0f84af
tangwulin <3498665850@qq.com>
2023-07-17 11:40:06 +0800

    使透明dock只能在tauri运行中开启

--- src/components/DevelopFeature.vue
+++ src/components/DevelopFeature.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { currentMonitor, WebviewWindow } from '@tauri-apps/api/window'
-import { NButton } from 'naive-ui'
+import { NButton, useMessage } from 'naive-ui'
 
+const message = useMessage()
+
+const isTauri = window.__TAURI_METADATA__ !== undefined
 const openDockWindow = async () => {
   const monitor = await currentMonitor()
   const width = monitor.size.width * 0.5
@@ -15,25 +18,35 @@ const openDockWindow = async () => {
     y: monitor.size.height * 0.75,
     transparent: true,
     decorations: false,
+    alwaysOnTop: true,
     skipTaskbar: true
   })
   await dock.once('tauri://created', function () {
     // webview window successfully created
+    message.success('dock已启动！')
   })
   await dock.once('tauri://error', function (e) {
     // an error occurred during webview window creation
+    message.error('dock创建失败！错误信息：' + JSON.stringify(e))
   })
 }
 
 const closeDockWindow = () => {
   const dock = WebviewWindow.getByLabel('dock')
-  dock.close()
+  if (dock === null)
+  {
+    message.error('dock未创建！')
+  }
+  else
+  {
+    dock.close()
+  }
 }
 </script>
 
 <template>
-  <n-button @click="openDockWindow">打开dock窗口</n-button>
-  <n-button @click="closeDockWindow">关闭dock窗口</n-button>
+  <n-button @click="openDockWindow" :disabled="!isTauri">打开dock窗口</n-button>
+  <n-button @click="closeDockWindow" :disabled="!isTauri">关闭dock窗口</n-button>
 </template>
 
 <style scoped>
