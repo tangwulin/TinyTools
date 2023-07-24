5109620b9fb992042a7c7dde2e7bbc024eb85dd5
tangwulin <3498665850@qq.com>
2023-07-24 23:14:48 +0800

    更改关闭窗口时的默认行为，转由webview层决定

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::WindowEvent;
 
 fn main() {
     tauri::Builder::default()
+        .on_window_event(|event| match event.event() {
+            WindowEvent::CloseRequested { api, .. } => {
+                println!("on CloseRequested");
+                api.prevent_close();
+                event.window().emit("close-requested", ()).unwrap();
+            }
+            _ => { }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
