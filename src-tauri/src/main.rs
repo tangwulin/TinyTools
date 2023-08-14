b9fd1cba1244dc1d7e45e919d5c862377f4962e1
tangwulin <3498665850@qq.com>
2023-08-14 13:44:41 +0800

    增加托盘相关

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use tauri::WindowEvent;
+mod tray;
 
 fn main() {
+
     tauri::Builder::default()
         .on_window_event(|event| match event.event() {
             WindowEvent::CloseRequested { api, .. } => {
@@ -12,6 +14,8 @@ fn main() {
             }
             _ => { }
         })
+        .system_tray(tray::menu())  // ✅ 将 `tauri.conf.json` 上配置的图标添加到系统托盘
+        .on_system_tray_event(tray::handler) // ✅ 注册系统托盘事件处理程序
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
