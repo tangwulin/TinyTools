efe423b016600eaa32ea52870be663d8f60ea2b5
tangwulin <3498665850@qq.com>
2023-08-17 17:50:47 +0800

    增加一些测试功能

--- src/components/DevelopFeature.vue
+++ src/components/DevelopFeature.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { useSettingStore } from '@/stores/setting'
 import { storeToRefs } from 'pinia'
-import { checkUpdate } from '@tauri-apps/api/updater';
+import { checkUpdate } from '@tauri-apps/api/updater'
+import { relaunch } from '@tauri-apps/api/process'
 import { currentMonitor, WebviewWindow } from '@tauri-apps/api/window'
 import { NButton, useMessage } from 'naive-ui'
 
 const setting = useSettingStore()
-const { enableDocking } = storeToRefs(setting)
+const { enableDocking, enableDevelopFeature } = storeToRefs(setting)
 
 const message = useMessage()
 
@@ -43,6 +44,7 @@ const closeDockWindow = () => {
   }
   else
   {
+    message.success('dock已关闭！')
     dock.close()
   }
 }
@@ -51,18 +53,43 @@ const checkUpdates = async () => {
   message.info('检查更新结果：' + JSON.stringify(update))
 }
 
+const disableDevelopFeature = () => {
+  enableDevelopFeature.value = false
+  relaunch()
+}
+
+const clearData = () => {
+  localStorage.clear()
+  message.success('数据已清除！程序即将重启！')
+  setTimeout(() => {
+    relaunch()
+  }, 1000)
+}
+
 </script>
 
 <template>
   <n-space vertical>
-    <n-space >
+    <n-space justify="space-between">
+      <n-space class="items-center">
+        <div>更改后请重启程序</div>
+        <n-button :disabled="!isTauri" type="primary" round @click="relaunch">重启</n-button>
+      </n-space>
+      <n-space class="items-center">
+        <n-button :disabled="!isTauri" type="error" round @click="clearData">清除数据</n-button>
+        <n-button :disabled="!isTauri" type="error" round @click="disableDevelopFeature">关闭调试功能</n-button>
+      </n-space>
+    </n-space>
+    <n-space class="items-center">
       <n-button :disabled="!isTauri" type="primary" round @click="openDockWindow">启动dock</n-button>
       <n-button :disabled="!isTauri" type="primary" round @click="closeDockWindow">关闭dock</n-button>
     </n-space>
-    <n-space>
+    <n-space class="items-center">
+      <p>手动检查更新</p>
       <n-button @click="checkUpdates">调用Invoke</n-button>
     </n-space>
-    <n-space>
+    <n-space class="items-center">
+      <p>启用dock栏</p>
       <n-switch v-model:value="enableDocking"/>
     </n-space>
   </n-space>
